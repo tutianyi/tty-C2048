cd963fbbb0290a8f1dd8d562af8b5baa3d0a92ea
Tianyi Tu <tutianyi@gmail.com>
2019-05-03 15:10:04 +0800

    完成棋盘模块的开始游戏、填数、打印棋盘三个操作

--- C2048/board.c
+++ C2048/board.c
@@ -1,13 +1,63 @@
 #include "board.h"
-
+#include <time.h>
+#include <stdlib.h>
 #define ROW 4
 #define COL 4
-int cells[ROW][COL];
+static int cells[ROW][COL];
 
-void StartGame(){}
-void InsertANumber(){}
-void Output(){}
-int MoveLeft(){return 1;}
-int MoveRight(){return 1;}
-int MoveUp(){return 1;}
-int MoveDown(){return 1;}
+void StartGame()
+{
+    int i,j;
+    for(i=0; i<ROW; i++)
+        for(j=0; j<COL; j++)
+            cells[i][j] = 0;
+}
+static void OutputLine(int row)
+{
+    int j;
+    for(j=0; j<COL; j++)
+    {
+        printf("©¦");
+        if(cells[row][j]==0)
+            printf("    ");
+        else
+            printf("%4d", cells[row][j]);
+    }
+    printf("©¦\n");
+}
+
+void Output()
+{
+    printf("©°©¤©¤©¤©¤©Ð©¤©¤©¤©¤©Ð©¤©¤©¤©¤©Ð©¤©¤©¤©¤©´\n");
+    OutputLine(0);
+    printf("©À©¤©¤©¤©¤©à©¤©¤©¤©¤©à©¤©¤©¤©¤©à©¤©¤©¤©¤©È\n");
+    OutputLine(1);
+    printf("©À©¤©¤©¤©¤©à©¤©¤©¤©¤©à©¤©¤©¤©¤©à©¤©¤©¤©¤©È\n");
+    OutputLine(2);
+    printf("©À©¤©¤©¤©¤©à©¤©¤©¤©¤©à©¤©¤©¤©¤©à©¤©¤©¤©¤©È\n");
+    OutputLine(3);
+    printf("©¸©¤©¤©¤©¤©Ø©¤©¤©¤©¤©Ø©¤©¤©¤©¤©Ø©¤©¤©¤©¤©¼\n");
+}
+
+void InsertANumber()
+{
+    srand((unsigned)time(NULL));
+    cells[rand()%ROW][rand()%COL] = 2;
+}
+
+int MoveLeft()
+{
+    return 1;
+}
+int MoveRight()
+{
+    return 1;
+}
+int MoveUp()
+{
+    return 1;
+}
+int MoveDown()
+{
+    return 1;
+}
